ed83d631de8192d041aeb63cc9b7d001447e2f81
lucagaddini <lucagaddini@icloud.com>
2022-09-23 15:06:24 +0200

    Aggiya Classe Address - Aggiunto Salvataggio dati da file ad una lista

---
+++ Address.cs
@@ -0,0 +1,41 @@
+﻿
+//Oggi esercitazione veloce, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe, in particolare nel live-coding di oggi.
+//In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe, 
+//e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
+//Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
+
+public class Address
+{
+    public string Name { get; }
+    public string Surname { get; }
+    public string Street { get; }
+    public string City { get; }
+    public string Province { get; }
+    public string Zip { get; }
+
+    public Address (string name, string surname,string street, string city, string province, string zip)
+    {
+        Name = name;
+        Surname = surname;
+        Street = street;
+        City = city;
+        Province = province;
+        Zip = zip;
+    }
+
+    public override string ToString()
+    {
+        string stringa = "------ Indirizzo ------\n";
+        stringa += "Nome:\t" + this.Name + "\n";
+        stringa += "Cognome:\t" + this.Surname + "\n";
+        stringa += "Via:\t" + this.Street + "\n";
+        stringa += "Città:\t" + this.City + "\n";
+        stringa += "Provincia:\t" + this.Province + "\n";
+        stringa += "ZIP:\t" + this.Zip + "\n";
+        stringa += "-------------------";
+
+        return stringa;
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -4,15 +4,73 @@
 //e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
 //Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
 
+using System.Net;
+
+List<Address> addressesList = new List<Address>();
+
+
+// LETTURA CSV ES SALVATAGGIO NELLA LISTA
+
 string path = "../../../addresses.csv";
+StreamReader addressFile = File.OpenText(path);
 
-// Open the file to read from.
-StreamReader file = File.OpenText(path);
+addressFile.ReadLine();
 
-while (!file.EndOfStream)
+
+while (!addressFile.EndOfStream)
 {
-    string riga = file.ReadLine();
-    Console.WriteLine(riga);
+    string row = addressFile.ReadLine();
+    string[] split = row.Split(',');
+
+    try
+    {
+        string name = split[0];
+        string surname = split[1];
+        string street = split[2];
+        string city = split[3];
+        string province = split[4];
+        string zip = split[5];
+
+        Address newAddress = new Address(name, surname, street, city, province, zip);
+
+        addressesList.Add(newAddress);
+
+        Console.WriteLine("Destinatario importato correttamente");
+
+    }
+
+    catch (IndexOutOfRangeException)
+    {
+        Console.WriteLine("Destinatario non valido -->" + row);
+    }
+  
 }
-    
-file.Close();
+
+addressFile.Close();
+
+
+// CREAZIONE FILE
+try
+{
+    StreamWriter addressOutputFile = File.CreateText("addressOutput.txt");
+
+
+    foreach (Address address in addressesList)
+    {
+        Console.WriteLine("Salvataggio {0}", address.Name);
+        addressOutputFile.WriteLine();
+        addressOutputFile.WriteLine(address.ToString());
+    }
+
+    addressOutputFile.Close();
+
+}
+catch (Exception e)
+{
+    Console.WriteLine("Qualcosa è andato storto: {0}", e.Message);
+}
+
+
+
+
+Console.WriteLine(" --- ", addressesList);
